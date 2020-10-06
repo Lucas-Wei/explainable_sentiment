7cb280134af3ed30740a588f0a02448d3e8fc3ac
Lucas-Wei <wbest26@gmail.com>
2020-10-06 15:56:48 -0400

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,14 @@ import os
 import numpy as np
 import dataset
 
+def get_selected_text(text, start_idx, end_idx, offsets):
+    selected_text = ""
+    for ix in range(start_idx, end_idx + 1):
+        selected_text += text[offsets[ix][0]: offsets[ix][1]]
+        if (ix + 1) < len(offsets) and offsets[ix][1] < offsets[ix + 1][0]:
+            selected_text += " "
+    return selected_text
+
 def jaccard(str1, str2):
     a = set(str1.lower().split())
     b = set(str2.lower().split())
