fe687a4dbd8ef6fd9da2cba449adb34c657b9532
Lucas-Wei <wbest26@gmail.com>
2020-10-06 00:53:26 -0400

    Update train.py

--- src/train.py
+++ src/train.py
@@ -8,9 +8,13 @@ from engine import train_fn
 config = configparser.ConfigParser()
 config.read('../config/config.ini')
 
+N_SPLITS = int(config['MODEL']['N_SPLITS'])
+BATCH_SIZE = int(config['MODEL']['BATCH_SIZE'])
+LR = float(config['MODEL']['LR'])
+TRAINING_FILE = config['PATHS']['TRAINING_FILE']
 
 def run()
-    skf = StratifiedKFold(n_splits=config[N_SPLITS], shuffle=True, random_state=seed)
+    skf = StratifiedKFold(n_splits=N_SPLITS, shuffle=True, random_state=seed)
     train_df = pd.read_csv(TRAINING_FILE)
     train_df['text'] = train_df['text'].astype(str)
     train_df['selected_text'] = train_df['selected_text'].astype(str)
@@ -18,9 +22,9 @@ def run()
     for fold, (train_idx, val_idx) in enumerate(skf.split(train_df, train_df.sentiment), start=1):
         print(f'Fold: {fold}')
         model = TweetModel()
-        optimizer = torch.optim.AdamW(model.parameters(), lr=config[LR], betas=(0.9, 0.999))
+        optimizer = torch.optim.AdamW(model.parameters(), lr=LR, betas=(0.9, 0.999))
         criterion = loss_fn
-        dataloaders_dict = get_train_val_loaders(train_df, train_idx, val_idx, int(config[MODEL][BATCH_SIZE])
+        dataloaders_dict = get_train_val_loaders(train_df, train_idx, val_idx, BATCH_SIZE)
         
         train_fn(
             model,
