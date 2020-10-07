8e7c680f1a39da653c508af116352c69fec52316
Lucas-Wei <wbest26@gmail.com>
2020-10-06 23:41:13 -0400

    Update inference.py

--- src/inference.py
+++ src/inference.py
@@ -9,12 +9,17 @@ import configparser
 config = configparser.ConfigParser()
 config.read('../config/config.ini')
 
+PTHS_PATH = config['PATHS']['PTHS_PATH']
+
 def get_selected_text(text, start_idx, end_idx, offsets):
-    selected_text = ""
-    for ix in range(start_idx, end_idx + 1):
-        selected_text += text[offsets[ix][0]: offsets[ix][1]]
-        if (ix + 1) < len(offsets) and offsets[ix][1] < offsets[ix + 1][0]:
-            selected_text += " "
+    if start_pred > end_pred:
+        selected_text = tweet
+    else:
+        selected_text = ""
+        for ix in range(start_idx, end_idx + 1):
+            selected_text += text[offsets[ix][0]: offsets[ix][1]]
+            if (ix + 1) < len(offsets) and offsets[ix][1] < offsets[ix + 1][0]:
+                selected_text += " "
     return selected_text
 
 def get_test_loader(df):
@@ -27,7 +32,7 @@ st.title('Explainable Sentiment')
 def build_model():
     model = TweetRoBERTaModel()
     model.cuda()
-    model.load_state_dict(torch.load(os.path.join(config['PATHS']['PTHS_PATH'], 'roberta_fold1.pth')))
+    model.load_state_dict(torch.load(os.path.join(PTHS_PATH, 'roberta_fold1.pth')))
     model.eval()
     return model
 
@@ -41,11 +46,10 @@ sentiment = st.selectbox(
 if input_text:
     d = {'text': input_text, 'sentiment': sentiment}
     df = pd.DataFrame(data=d, index=[0])
-    # st.write(df)
     df['text'] = df['text'].astype(str)
     test_loader = get_test_loader(df)
-
     data = next(iter(test_loader))
+    
     ids = data['ids'].cuda()
     masks = data['masks'].cuda()
     tweet = data['tweet']
@@ -58,9 +62,7 @@ if input_text:
     for i in range(len(ids)):
         start_pred = np.argmax(start_logits)
         end_pred = np.argmax(end_logits)
-        if start_pred > end_pred:
-            pred = tweet
-        else:
-            pred = get_selected_text(tweet[i], start_pred, end_pred, offsets[i])
+        pred = get_selected_text(tweet[i], start_pred, end_pred, offsets[i])
+
     st.text('Words explain sentiment:')
     st.write(pred)
