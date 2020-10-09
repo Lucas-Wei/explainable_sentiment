8fe74f0e768fcd7ae6c4105bc6e91b7e77e66ba4
Lucas-Wei <wbest26@gmail.com>
2020-10-09 00:51:28 -0400

    Update inference.py

--- src/inference.py
+++ src/inference.py
@@ -71,4 +71,5 @@ if input_text:
 uploaded_file = st.file_uploader("Choose a file(.csv)", type='csv')
 if uploaded_file:
     # df = pd.read_csv(uploaded_file)
-    st.write(uploaded_file)
+    text_io = io.TextIOWrapper(uploaded_file)
+    st.write(text_io)
