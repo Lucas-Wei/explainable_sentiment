e4ee9bc5680b75dead4b78e3e1d319366c0dd4de
Lucas-Wei <wbest26@gmail.com>
2020-10-09 14:41:57 -0400

    Update inference.py

--- src/inference.py
+++ src/inference.py
@@ -8,7 +8,7 @@ import dataset
 import io
 
 def get_selected_text(text, start_idx, end_idx, offsets):
-    if start_idx > end_idx
+    if start_idx > end_idx:
         selected_text = tweet
     else:
         selected_text = ""
