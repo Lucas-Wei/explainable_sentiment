5c05bcc500f391d0cd3f274cea29fdaf4d510a54
Lucas-Wei <wbest26@gmail.com>
2020-10-12 22:27:53 -0400

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -7,7 +7,7 @@ def get_selected_text(text, start_idx, end_idx, offsets):
     if start_idx > end_idx:
         selected_text = text
     else:
-    selected_text = ""
+        selected_text = ""
     for ix in range(start_idx, end_idx + 1):
         selected_text += text[offsets[ix][0]: offsets[ix][1]]
         if (ix + 1) < len(offsets) and offsets[ix][1] < offsets[ix + 1][0]:
